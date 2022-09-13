f57e873f0bcf5147dcdc3c8ea0f5ba6c62b1a34a
AndrewWalczyk <86196697+AndrewWalczyk@users.noreply.github.com>
2022-09-13 16:40:29 -0400

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -15,11 +15,7 @@ data "oci_identity_tenancy" "tenant_details" {
 }
 
 data "oci_identity_regions" "home_region" {
-  filter {
-    name   = "key"
-    values = [data.oci_identity_tenancy.tenant_details.home_region_key]
-  }
-
+  region = var.region
   provider = oci.current_region
 }
 
